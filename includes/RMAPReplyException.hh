224979a2a7ffccf7594af43fcbf7fdca38193fd4
Takayuki Yuasa <yuasatakayuki@gmail.com>
2011-12-16 20:04:33 +0100

    Reply status was totally moved to RMAPReplyStatus class. RMAPReplyStatus::replyStatusToString() should be used to convert uint8_t reply status to String.

--- includes/RMAPReplyException.hh
+++ includes/RMAPReplyException.hh
@@ -19,38 +19,9 @@ public:
 		CxxUtilities::Exception(status) {
 	}
 
+public:
 	std::string toString() {
-		switch (status) {
-		case RMAPReplyStatus::CommandExcecutedSuccessfully:
-			return "CommandExcecutedSuccessfully (0x00)";
-		case RMAPReplyStatus::GeneralError:
-			return "GeneralError (0x01)";
-		case RMAPReplyStatus::UnusedRMAPPacketTypeOrCommandCode:
-			return "UnusedRMAPPacketTypeOrCommandCode (0x02)";
-		case RMAPReplyStatus::InvalidDestinationKey:
-			return "InvalidDestinationKey (0x03)";
-		case RMAPReplyStatus::InvalidDataCRC:
-			return "InvalidDataCRC (0x04)";
-		case RMAPReplyStatus::EarlyEOP:
-			return "EarlyEOP (0x05)";
-		case RMAPReplyStatus::CargoTooLarge:
-			return "CargoTooLarge (0x06)";
-		case RMAPReplyStatus::EEP:
-			return "EEP (0x07)";
-		case RMAPReplyStatus::VerifyBufferOverrun:
-			return "VerifyBufferOverrun (0x08)";
-		case RMAPReplyStatus::CommandNotImplementedOrNotAuthorized:
-			return "CommandNotImplementedOrNotAuthorized (0x0a)";
-		case RMAPReplyStatus::RMWDataLengthError:
-			return "RMWDataLengthError (0x0b)";
-		case RMAPReplyStatus::InvalidDataDestinationLogicalAddress:
-			return "InvalidDataDestinationLogicalAddress (0x0c)";
-		default:
-			using namespace std;
-			std::stringstream ss;
-			ss << "Undefined status value (0x" << right << setw(2) << setfill('0') << hex << status << ")";
-			return ss.str();
-		}
+		return RMAPReplyStatus::replyStatusToString(this->status);
 	}
 };
 
--- includes/RMAPReplyStatus.hh
+++ includes/RMAPReplyStatus.hh
@@ -24,6 +24,55 @@ public:
 		RMWDataLengthError=0x0b,
 		InvalidDataDestinationLogicalAddress=0x0c
 	};
+	static std::string replyStatusToString(uint8_t status) {
+		using namespace std;
+		//Status
+		string statusstring;
+		stringstream ss;
+		switch (status) {
+		case 0x00:
+			statusstring = "Successfully Executed (0x00)";
+			break;
+		case 0x01:
+			statusstring = "General Error (0x01)";
+			break;
+		case 0x02:
+			statusstring = "Unused RMAP Packet Type or Command Code (0x02)";
+			break;
+		case 0x03:
+			statusstring = "Invalid Target Key (0x03)";
+			break;
+		case 0x04:
+			statusstring = "Invalid Data CRC (0x04)";
+			break;
+		case 0x05:
+			statusstring = "Early EOP (0x05)";
+			break;
+		case 0x06:
+			statusstring = "Cargo Too Large (0x06)";
+			break;
+		case 0x07:
+			statusstring = "EEP (0x07)";
+			break;
+		case 0x08:
+			statusstring = "Reserved (0x08)";
+			break;
+		case 0x09:
+			statusstring = "Verify Buffer Overrun (0x09)";
+			break;
+		case 0x0a:
+			statusstring = "RMAP Command Not Implemented or Not Authorized (0x0a)";
+			break;
+		case 0x0b:
+			statusstring = "Invalid Target Logical Address (0x0b)";
+			break;
+		default:
+			ss << "Reserved (" << "0x" << hex << right << setw(2) << setfill('0') << (uint32_t) status << ")";
+			statusstring = ss.str();
+			break;
+		}
+		return statusstring;
+	}
 };
 
 #endif /* RMAPREPLYSTATUS_HH_ */
