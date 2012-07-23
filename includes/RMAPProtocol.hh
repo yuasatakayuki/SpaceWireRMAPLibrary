742ee57915eecf343c5c3065e86c5ab559d059d8
Takayuki Yuasa <yuasatakayuki@gmail.com>
2012-07-23 22:34:30 +0900

    typo in RMAPReplyStatus.hh was fixed

--- includes/RMAPProtocol.hh
+++ includes/RMAPProtocol.hh
@@ -48,23 +48,6 @@ public:
 
 	static const uint8_t DefaultLogicalAddress = 0xFE;
 
-	/* Use RMAPReplyStatus class insted. */
-	/*
-	 static enum {
-	 CommandExcecutedSuccessfully = 0x00,
-	 GeneralError = 0x01,
-	 UnusedRMAPPacketTypeOrCommandCode = 0x02,
-	 InvalidDestinationKey = 0x03,
-	 InvalidDataCRC = 0x04,
-	 EarlyEOP = 0x05,
-	 CargoTooLarge = 0x06,
-	 EEP = 0x07,
-	 VerifyBufferOverrun = 0x09,
-	 CommandNotImplementedOrNotAuthorized = 0x0a,
-	 RMWDataLengthError = 0x0b,
-	 InvalidDataDestinationLogicalAddress = 0x0c
-	 } ReplyStatus;
-	 */
 };
 
 #endif /* RMAPPROTOCOL_HH_ */
