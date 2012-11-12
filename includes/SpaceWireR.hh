26c95a834730222a9078c35d9db7ded0ae936f91
Takayuki Yuasa <yuasatakayuki@gmail.com>
2012-11-13 00:54:53 +0900

    first import of SpaceWire-R header file.

---
+++ includes/SpaceWireR.hh
@@ -0,0 +1,16 @@
+/*
+ * SpaceWireR.hh
+ *
+ *  Created on: Oct 8, 2012
+ *      Author: yuasa
+ */
+
+#ifndef SPACEWIRER_HH_
+#define SPACEWIRER_HH_
+
+#include "SpaceWire.hh"
+#include "SpaceWireR/SpaceWireRProtocol.hh"
+#include "SpaceWireR/SpaceWireRPacket.hh"
+
+
+#endif /* SPACEWIRER_HH_ */
