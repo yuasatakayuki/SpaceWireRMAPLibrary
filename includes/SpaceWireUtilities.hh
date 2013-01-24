c3c6b78aa48768796e101393c7f5fd6d1c8038d8
Takayuki Yuasa <yuasatakayuki@gmail.com>
2013-01-24 23:18:10 +0900

    Doxygen comment was added to SpaceWireUtilities.hh

--- includes/SpaceWireUtilities.hh
+++ includes/SpaceWireUtilities.hh
@@ -10,19 +10,28 @@
 
 #include "CxxUtilities/CxxUtilities.hh"
 
+/** An exception class that is used by the SpaceWireUtilities class. */
 class SpaceWireUtilitiesException: public CxxUtilities::Exception {
 public:
 	enum {
 		SizeIncorrect
 	};
+
 public:
 	SpaceWireUtilitiesException(uint32_t status) :
 			CxxUtilities::Exception(status) {
 	}
+
+public:
+	virtual ~SpaceWireUtilitiesException() {
+	}
 };
 
+/** A class that provides utility methods used in SpaceWire RMAP Library. */
 class SpaceWireUtilities {
 public:
+	/** Concatenates two std::vector<uint8_t> instances creating new instance.
+	 */
 	static std::vector<uint8_t> concatenate(std::vector<uint8_t>& array1, std::vector<uint8_t>& array2) {
 		std::vector<uint8_t> result;
 		for (size_t i = 0; i < array1.size(); i++) {
@@ -34,6 +43,9 @@ public:
 		return result;
 	}
 
+public:
+	/** Concatenates two std::vector<uint8_t> instances creating new instance.
+	 */
 	static void concatenateTo(std::vector<uint8_t>& array1, std::vector<uint8_t>& array2) {
 		using namespace std;
 		for (size_t i = 0; i < array2.size(); i++) {
@@ -42,31 +54,44 @@ public:
 	}
 
 public:
+	/** Dumps packet content to the screen. */
 	static void dumpPacketUnsignedChar(std::vector<unsigned char>& data) throw (SpaceWireUtilitiesException) {
-		std::vector < uint8_t > data2(data.size());
+		std::vector<uint8_t> data2(data.size());
 		for (size_t i = 0; i < data.size(); i++) {
 			data2[i] = data[i];
 		}
 		dumpPacket(&std::cout, &data2, 1);
 	}
 
+public:
+	/** Dumps packet content to the screen. */
 	static void dumpPacket(std::vector<uint8_t>& data) throw (SpaceWireUtilitiesException) {
 		dumpPacket(&std::cout, &data, 1);
 	}
 
+public:
+	/** Dumps packet content to the screen. */
 	static void dumpPacket(std::vector<uint8_t>* data) throw (SpaceWireUtilitiesException) {
 		dumpPacket(&std::cout, data, 1);
 	}
 
+public:
+	/** Dumps packet content to the screen. */
 	static void dumpPacket(std::ostream* ofs, std::vector<uint8_t>* data) throw (SpaceWireUtilitiesException) {
 		dumpPacket(ofs, data, 1);
 	}
 
-	static void dumpPacket(std::vector<uint8_t>* data, uint32_t wordwidth) throw (SpaceWireUtilitiesException) {
+public:
+	/** Dumps packet content to the screen. */
+	static void dumpPacket(std::vector<uint8_t>* data, size_t wordwidth) throw (SpaceWireUtilitiesException) {
 		dumpPacket(&std::cout, data, wordwidth);
 	}
 
-	static void dumpPacket(std::ostream* ofs, std::vector<uint8_t>* data, uint32_t wordwidth, uint32_t bytesPerLine =
+public:
+	/** Dumps packet content to an output stream instance.
+	 * @param[in] output stream to which dump is performed.
+	 */
+	static void dumpPacket(std::ostream* ofs, std::vector<uint8_t>* data, uint32_t wordwidth, size_t bytesPerLine =
 			SpaceWireUtilities::DumpsPerLine) throw (SpaceWireUtilitiesException) {
 		using namespace std;
 		int v;
@@ -76,11 +101,11 @@ public:
 		if (data->size() > wordwidth && data->size() % wordwidth != 0) {
 			throw(SpaceWireUtilitiesException(SpaceWireUtilitiesException::SizeIncorrect));
 		}
-		for (uint32_t i = 0; i < (data->size() / wordwidth + bytesPerLine - 1) / bytesPerLine; i++) {
-			for (uint32_t o = 0; o < bytesPerLine; o++) {
+		for (size_t i = 0; i < (data->size() / wordwidth + bytesPerLine - 1) / bytesPerLine; i++) {
+			for (size_t o = 0; o < bytesPerLine; o++) {
 				if (i * bytesPerLine + o < data->size() / wordwidth) {
 					v = 0;
-					for (uint32_t p = 0; p < wordwidth; p++) {
+					for (size_t p = 0; p < wordwidth; p++) {
 						v = v + (uint32_t) data->at((i * bytesPerLine + o) * wordwidth + p);
 					}
 					(*ofs) << "0x" << right << hex << setw(2 * wordwidth) << setfill('0') << v << " ";
@@ -90,8 +115,12 @@ public:
 		}
 	}
 
-	static void dumpPacket(std::ostream* ofs, uint8_t* data, size_t length, uint32_t wordwidth = 16,
-			uint32_t bytesPerLine = SpaceWireUtilities::DumpsPerLine) throw (SpaceWireUtilitiesException) {
+public:
+	/** Dumps packet content to an output stream instance.
+	 * @param[in] output stream to which dump is performed.
+	 */
+	static void dumpPacket(std::ostream* ofs, uint8_t* data, size_t length, size_t wordwidth = 16,
+			size_t bytesPerLine = SpaceWireUtilities::DumpsPerLine) throw (SpaceWireUtilitiesException) {
 		using namespace std;
 		int v;
 		if (wordwidth == 0) {
@@ -100,11 +129,11 @@ public:
 		if (length > wordwidth && length % wordwidth != 0) {
 			throw(SpaceWireUtilitiesException(SpaceWireUtilitiesException::SizeIncorrect));
 		}
-		for (uint32_t i = 0; i < (length / wordwidth + bytesPerLine - 1) / bytesPerLine; i++) {
-			for (uint32_t o = 0; o < bytesPerLine; o++) {
+		for (size_t i = 0; i < (length / wordwidth + bytesPerLine - 1) / bytesPerLine; i++) {
+			for (size_t o = 0; o < bytesPerLine; o++) {
 				if (i * bytesPerLine + o < length / wordwidth) {
 					v = 0;
-					for (uint32_t p = 0; p < wordwidth; p++) {
+					for (size_t p = 0; p < wordwidth; p++) {
 						v = v + (uint32_t) data[(i * bytesPerLine + o) * wordwidth + p];
 					}
 					(*ofs) << "0x" << right << hex << setw(2 * wordwidth) << setfill('0') << v << " ";
@@ -114,6 +143,8 @@ public:
 		}
 	}
 
+public:
+	/** Converts a packet content to std::string. */
 	static std::string packetToString(std::vector<uint8_t>* data, size_t nBytesDisplayed = 16)
 			throw (SpaceWireUtilitiesException) {
 		if (data->size() == 0) {
@@ -122,11 +153,13 @@ public:
 		return packetToString(&(data->at(0)), data->size(), nBytesDisplayed);
 	}
 
+public:
+	/** Converts a packet content to std::string. */
 	static std::string packetToString(uint8_t* data, int length, size_t nBytesDisplayed = 16)
 			throw (SpaceWireUtilitiesException) {
 		using namespace std;
 		size_t max;
-		if (nBytesDisplayed < (unsigned int) length) {
+		if (nBytesDisplayed < length) {
 			max = nBytesDisplayed;
 		} else {
 			max = length;
@@ -147,6 +180,8 @@ public:
 		return ss.str();
 	}
 
+public:
+	/** Converts string to uint8_t. */
 	static uint8_t convertStringToUnsignedChar(std::string str) {
 		using namespace std;
 		int data = 0;
@@ -159,10 +194,15 @@ public:
 		return (uint8_t) data;
 	}
 
+public:
+	/** Dumps a packet content with addresses to the screen.
+	 * @param address initial address.
+	 * @param data packet content to be dumped.
+	 */
 	static void printVectorWithAddress(uint32_t address, std::vector<uint8_t>* data) {
 		using namespace std;
 		cout << "Address    Data" << endl;
-		for (uint32_t i = 0; i < data->size(); i++) {
+		for (size_t i = 0; i < data->size(); i++) {
 			cout << setfill('0') << setw(4) << hex << (address + i) / 0x00010000;
 			cout << "-" << setfill('0') << setw(4) << hex << (address + i) % 0x00010000;
 			cout << "  ";
@@ -171,6 +211,11 @@ public:
 		cout << dec << left;
 	}
 
+public:
+	/** Dumps a packet content with addresses to the screen.
+	 * @param address initial address.
+	 * @param data packet content to be dumped.
+	 */
 	static void printVectorWithAddress2bytes(uint32_t address, std::vector<uint8_t>* data) {
 		using namespace std;
 		cout << "Address    Data" << endl;
@@ -201,6 +246,6 @@ public:
 	}
 
 public:
-	static const uint32_t DumpsPerLine = 8;
+	static const size_t DumpsPerLine = 8;
 };
 #endif /* SPACEWIREUTILITIES_HH_ */
