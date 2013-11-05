32339cfa0c28999b6f055d0de2a585336fa29fed
Takayuki Yuasa <yuasatakayuki@gmail.com>
2013-11-05 14:26:45 +0900

    RMAPEngine.hh - a bug in transaction ID management was fixed (no management is necessary in RMAP Write without Reply)

--- includes/RMAPEngine.hh
+++ includes/RMAPEngine.hh
@@ -1,29 +1,29 @@
 /* 
-============================================================================
-SpaceWire/RMAP Library is provided under the MIT License.
-============================================================================
+ ============================================================================
+ SpaceWire/RMAP Library is provided under the MIT License.
+ ============================================================================
 
-Copyright (c) 2006-2013 Takayuki Yuasa and The Open-source SpaceWire Project
+ Copyright (c) 2006-2013 Takayuki Yuasa and The Open-source SpaceWire Project
 
-Permission is hereby granted, free of charge, to any person obtaining a 
-copy of this software and associated documentation files (the 
-"Software"), to deal in the Software without restriction, including 
-without limitation the rights to use, copy, modify, merge, publish, 
-distribute, sublicense, and/or sell copies of the Software, and to 
-permit persons to whom the Software is furnished to do so, subject to 
-the following conditions:
+ Permission is hereby granted, free of charge, to any person obtaining a
+ copy of this software and associated documentation files (the
+ "Software"), to deal in the Software without restriction, including
+ without limitation the rights to use, copy, modify, merge, publish,
+ distribute, sublicense, and/or sell copies of the Software, and to
+ permit persons to whom the Software is furnished to do so, subject to
+ the following conditions:
 
-The above copyright notice and this permission notice shall be included 
-in all copies or substantial portions of the Software.
+ The above copyright notice and this permission notice shall be included
+ in all copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
-OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
-IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY 
-CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
-TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
-SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. 
-*/
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 /*
  * RMAPEngine.hh
  *
@@ -357,8 +357,7 @@ private:
 			RMAPTargetAccessAction* rmapTargetAcessAction = rmapTargets[i]->getCorrespondingRMAPTargetAccessAction(
 					&rmapTransaction);
 			if (rmapTargetAcessAction != NULL) {
-				RMAPTargetProcessThread* aThread = new RMAPTargetProcessThread(this, rmapTransaction,
-						rmapTargetAcessAction);
+				RMAPTargetProcessThread* aThread = new RMAPTargetProcessThread(this, rmapTransaction, rmapTargetAcessAction);
 				aThread->start();
 				rmapTargetProcessThreads.push_back(aThread);
 				return;
@@ -497,9 +496,12 @@ public:
 			}
 		}
 		//register the transaction to management list
-		transactionIDMutex.lock();
-		transactions[transactionID] = transaction;
-		transactionIDMutex.unlock();
+		//if Reply is required
+		if (transaction->commandPacket->isReplyFlagSet()) {
+			transactionIDMutex.lock();
+			transactions[transactionID] = transaction;
+			transactionIDMutex.unlock();
+		}
 		//send a command packet
 		commandPacket->setTransactionID(transactionID);
 		commandPacket->constructPacket();
@@ -600,8 +602,8 @@ public:
 		rmapTargets.push_back(rmapTarget);
 	}
 
-
-public:	void removeRMAPTarget(RMAPTarget* rmapTarget) {
+public:
+	void removeRMAPTarget(RMAPTarget* rmapTarget) {
 		std::vector<RMAPTarget*> aVector;
 		for (size_t i = 0; i < rmapTargets.size(); i++) {
 			if (rmapTargets[i] != rmapTarget) {
@@ -616,13 +618,13 @@ public:
 		rmapEngineStoppedActions.addAction(rmapEngineStoppedAction);
 	}
 
-
-public:	void removeRMAPEngineStoppedAction(RMAPEngineStoppedAction* rmapEngineStoppedAction) {
+public:
+	void removeRMAPEngineStoppedAction(RMAPEngineStoppedAction* rmapEngineStoppedAction) {
 		rmapEngineStoppedActions.removeAction(rmapEngineStoppedAction);
 	}
 
-
-public:	CxxUtilities::Actions<void>* getRMAPEngineStoppedActions() {
+public:
+	CxxUtilities::Actions<void>* getRMAPEngineStoppedActions() {
 		return &rmapEngineStoppedActions;
 	}
 
@@ -640,8 +642,8 @@ public:
 		return useDraftECRC;
 	}
 
-
-public:	void setUseDraftECRC(bool useDraftEcrc = true) {
+public:
+	void setUseDraftECRC(bool useDraftEcrc = true) {
 		this->useDraftECRC = useDraftEcrc;
 	}
 
